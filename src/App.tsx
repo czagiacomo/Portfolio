c852ce49e739f4bd1350e667594967edbc476fda
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-01-17 14:23:16 -0300

    ✨ feat: Scroll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
-import Contact from './pages/contact/Contact';
-import Projects from './pages/projects/Projects';
-import BlogPessoal from './pages/projects/BlogPessoal';
-import CRM from './pages/projects/CRM';
-import Name from './pages/projects/ProjectTemplate';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import Projects from "./pages/projects/Projects";
+import BlogPessoal from "./pages/projects/BlogPessoal";
+import CRM from "./pages/projects/CRM";
+import Name from "./pages/projects/ProjectTemplate";
+import UpButton from "./components/upbutton/UpButton";
 
 export default function App() {
   return (
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/blogpessoal" element={<BlogPessoal />} />
-            <Route path="/CRM" element={<CRM />} />
-            <Route path="/ProjectTemplate" element={<Name />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blogpessoal" element={<BlogPessoal />} />
+          <Route path="/CRM" element={<CRM />} />
+          <Route path="/ProjectTemplate" element={<Name />} />
+        </Routes>
+      </div>
+      <UpButton />
+      <Footer />
+    </BrowserRouter>
   );
 }
