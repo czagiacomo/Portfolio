5a6e6769349bae4cd89d984538f6145607177d01
czagiacomo <catharina.zagiacomo@gmail.com>
2025-03-18 19:08:14 -0300

    Update BootcampGeneration.tsx

--- src/pages/bootcamp/BootcampGeneration.tsx
+++ src/pages/bootcamp/BootcampGeneration.tsx
@@ -4,7 +4,7 @@ import {
   LinkedinLogo,
   StarFour,
 } from "@phosphor-icons/react";
-import React from "react";
+import ArticlePage from "../../components/article/Article";
 
 function BootcampGeneration() {
   return (
@@ -23,38 +23,7 @@ function BootcampGeneration() {
       */}
         </div>
 
-        <div className="max-w-4xl mx-auto my-6 px-6">
-          <h1 className="text-4xl font-bold mb-4 font-viga mt-10">
-            Bootcamp da Generation Brasil
-          </h1>
-          <div className="flex justify-between text-gray-600 text-sm mb-4">
-            <div>
-              <b>Autora - Catharina Zagiacomo (Dev BackEnd)</b>
-            </div>
-            <div>00/00/0000</div>
-          </div>
-          <div className="flex space-x-3 mb-6 mt-6">
-            <a href="https://linkedin.com/in/czagiacomo/" target="_blank">
-              <LinkedinLogo size={32} color="#0e76a8 " weight="fill" />
-            </a>
-            <a href="https://github.com/czagiacomo" target="_blank">
-              <GithubLogo size={30} weight="fill" />
-            </a>
-            <a href="https://brazil.generation.org" target="_blank">
-              <Globe size={30} weight="regular" color="#0d78d2" />
-            </a>
-          </div>
-          <article>
-            <p className="text-lg leading-7 mb-4">
-              Eu ainda estou trabalhando nesse artigo...
-            </p>
-            <img
-              className=" object-cover object-bottom max-w-6xl mx-auto"
-              src="https://cdn.sanity.io/images/do2rqv0h/production/3356021b2d743e60cb89b0b97196fb2b2b0b44a0-800x800.gif?w=1116&fit=max&auto=format"
-              alt="post-img"
-            />
-          </article>
-        </div>
+        <ArticlePage slug={"generation-brasil"} />
         <hr className="max-w-6xl mx-auto" />
       </div>
     </>
